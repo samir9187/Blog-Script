dac07a3544ef5fd5232d361f05c78e742658c30d
Samir Kumar Kunwar <86612382+samir9187@users.noreply.github.com>
2025-03-20 18:56:31 +0530

    Update multer.js

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,12 +1,11 @@
+
+
 import multer from 'multer';
 
 const storage = multer.memoryStorage();
 
 const upload = multer({
     storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 2,
-    },
-  }).single('File');
-
+    limits: { fileSize: 1024 * 1024 * 2 },
+}).single('Cover');  
 export default upload;
